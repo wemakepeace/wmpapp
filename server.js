5f05b5032430f70668fae109d559a67e138f5415
Kristine Alnes <kris.alnes@gmail.com>
2018-03-12 16:12:10 -0400

    Hot reload on both server and client

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(require('webpack-dev-middleware')(compiler, {
     publicPath: config.output.publicPath
 }));
 
+app.use(require('webpack-hot-middleware')(compiler));
+
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
