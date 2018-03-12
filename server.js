765edbfeaa19d9c6aed98c79ae45a15621fa171e
Kristine Alnes <kris.alnes@gmail.com>
2018-03-12 14:09:13 -0400

    Server is initiating webpack bundle

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const path = require('path');
 const bodyparser = require('body-parser');
+const open = require('open');
 // const db = require('./server/db');
 
+const webpack =  require('webpack');
+const config =  require('./webpack.config');
+
+const compiler = webpack(config);
 const app = express();
+
+app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+}));
+
+
+
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
@@ -16,13 +29,21 @@ app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/css', express.static(path.join(__dirname, 'client/css')));
 
 app.get('*', (req, res, next) => {
-    return res.sendFile(path.join(__dirname, 'index.html'));
+    return res.sendFile(path.join(__dirname, '/client/src/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`${port} is a beautiful port.`));
+// app.listen(port, () => console.log(`${port} is a beautiful port.`));
+app.listen(port, function (error) {
+    if(error) {
+        console.log(error);
+    } else {
+        open(`http://localhost:${port}`)
+    }
+});
 
-console.log(db)
+
+// console.log(db)
 // seed here later
 // db.seed();
