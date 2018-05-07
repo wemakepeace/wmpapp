f1ae097dbfdd39da16f1c1cc33abc7f751588d41
Kristine Alnes <kris.alnes@gmail.com>
2018-05-07 15:04:53 -0400

    Finds the match class that is the longest away

--- server/api/exchange.js
+++ server/api/exchange.js
@@ -18,16 +18,36 @@ const googleMapsClient = require('@google/maps').createClient({
 
 app.post('/', (req, res, next) => {
     const { classId } = req.body;
-    Class.findById(classId)
+    Class.findOne({
+        where: { id: classId },
+        include: [ School ]
+    })
     .then(_class => {
         _class = _class.dataValues;
+        _class.school = _class.school.dataValues;
+        let classData = extractClassData(_class);
+        // console.log('classData', classData)
+        return googleMapsClient.geocode({ address: classData.address })
+            .asPromise()
+            .then((response) => {
+
+                _class.location = response.json.results[0].geometry.location
+                return _class
+            })
+            .catch((err) => {
+                // [TODO] handle error
+                console.log(err);
+            })
+        })
+    .then(_class => {
+
         Exchange.findAll({
             where: {
                 status: 'initiated'
             },
             include: [{
                 model: Class,
-                where: { id: { $notIn: classId }},
+                where: { id: { $notIn: _class.id }},
                 include: [ School ]
             }]
         })
@@ -48,28 +68,14 @@ app.post('/', (req, res, next) => {
                 return  exchange
             })
 
-            const classData = matchingClasses.map(match => {
-                let matchClass = match.dataValues.classes[0].dataValues;
-                let data;
-
-                const zip = matchClass.school.zip
-                const countryCode =  matchClass.school.country;
-                const address1 = matchClass.school.address1;
-                const city = matchClass.school.city;
-                const country = countries().getName(countryCode);
-                const address = `${address1}, ${city}, ${country}`;
-
-                data = {
-                    id: matchClass.id,
-                    address: address
-                }
-
-                return data
+            const classDataCollection = matchingClasses.map(match => {
+                const data = match.dataValues.classes[0].dataValues;
+                return extractClassData(data)
             });
             // if more than one class
             if (matchingClasses.length > 1) {
 
-                Promise.all(classData.map(data => {
+                Promise.all(classDataCollection.map(data => {
                     // '1600 Amphitheatre Parkway, Mountain View, CA'
                     return googleMapsClient.geocode({address: data.address})
                     .asPromise()
@@ -82,9 +88,27 @@ app.post('/', (req, res, next) => {
                         console.log(err);
                     });
                 }))
-                .then(result => {
-                    result.map(x => console.log('x', x))
-                    res.send(result)
+                .then(dataWithCoords => {
+                    dataWithCoords.map(x => console.log('x', x))
+                    let matchClass;
+                    console.log('_class', _class)
+                    console.log('dataWithCoords', dataWithCoords)
+
+                    if (dataWithCoords.length) {
+                        const _classCoords = _class.location;
+                        matchClass = dataWithCoords.reduce((result, curr) => {
+                            const currCoords = curr.location;
+                            const distance =  calculateDistance(_classCoords, currCoords);
+                            console.log('distance', distance)
+                            if (distance > result.distance) {
+                                result.id = curr.id;
+                                result.distance = distance;
+                            }
+                            return result
+                        }, { id: null, distance: 0})
+                    }
+                    console.log('matchClass', matchClass)
+                    res.send(matchClass)
                 })
             }
                 // create verify tokens for both classes
@@ -120,6 +144,21 @@ app.post('/', (req, res, next) => {
 
 module.exports = app;
 
+const extractClassData = (_class) => {
+    let data;
+    const zip = _class.school.zip
+    const countryCode =  _class.school.country;
+    const address1 = _class.school.address1;
+    const city = _class.school.city;
+    const country = countries().getName(countryCode);
+    const address = `${address1}, ${city}, ${country}`;
+
+    data = {
+        id: _class.id,
+        address: address
+    }
+    return data
+}
 
 const getCoords = (match) => {
      let matchClass = match.dataValues.classes[0].dataValues;
@@ -145,6 +184,7 @@ const getCoords = (match) => {
         console.log(err);
     });
 }
+
 
 const findMatchFn = (schoolDetails) => {
     return (dispatch)=> {
@@ -175,13 +215,14 @@ const findMatchFn = (schoolDetails) => {
 }
 
 /* helper fn that calculates distance between coordinates */
-const distanceX = (lat1, lon1, lat2, lon2) => {
-    const radlat1 = Math.PI * lat1 / 180
-    const radlat2 = Math.PI * lat2 / 180
-    const radlon1 = Math.PI * lon1 / 180
-    const radlon2 = Math.PI * lon2 / 180
-    const theta = lon1 - lon2
-    const radtheta = Math.PI * theta/180
+// const calculateDistance = (lat1, lon1, lat2, lon2) => {
+    const calculateDistance = (location1, location2) => {
+    const radlat1 = Math.PI * location1.lat / 180
+    const radlat2 = Math.PI * location2.lat / 180
+    const radlon1 = Math.PI * location1.lng / 180
+    const radlon2 = Math.PI * location2.lng / 180
+    const theta = location1.lng - location2.lng
+    const radtheta = Math.PI * theta / 180
     let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
     dist = Math.acos(dist)
     dist = dist * 180/Math.PI
