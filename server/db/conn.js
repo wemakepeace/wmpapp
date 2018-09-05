04cf85d48c7e9059ed3fa28408c96246dbccd2dd
Kristine Alnes <kris.alnes@gmail.com>
2018-09-05 11:57:13 -0400

    Added dialect to Sequelize initiatoin

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const conn = new Sequelize(process.env.DATABASE_URL);
+const conn = new Sequelize(process.env.DATABASE_URL, { dialect: 'postgres' });
 
 module.exports = conn;
